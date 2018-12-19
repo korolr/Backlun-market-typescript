f0e77d55dbf061b808b9ebf6b2d014443b2f8836
Alexsey <ekstazu@gmail.com>
2018-12-20 01:07:06 +0400

    change to take effect

--- src/sagas/basketSagas.tsx
+++ src/sagas/basketSagas.tsx
@@ -9,63 +9,89 @@ import {
   BASKET_UPDATE,
   BASKET_FETCHED,
 } from "../actions/basketActions"
-import { call, put, takeEvery, select, ForkEffect } from "redux-saga/effects"
+import { take, call, put, select } from "redux-saga/effects"
 
 const getToken = (state: rootState) => state.login.token
 
-export function* watchBuyBasket(): IterableIterator<ForkEffect> {
-  yield takeEvery(BASKET_BUY, buyBasketAsync)
-}
-
-export function* buyBasketAsync() {
-  try {
-    yield put(requestBasket())
-    const token = yield select(getToken)
-    const basket = yield call(() => {
-      return HTTP.post(
-        `api/market/pay`,
-        {},
-        {
-          params: {
-            token: token,
-          },
-        }
-      ).then(response => {
-        return []
+export function* watchBuyBasket() {
+  while (true) {
+    yield take(BASKET_BUY)
+    try {
+      yield put(requestBasket())
+      const token = yield select(getToken)
+      const basket = yield call(() => {
+        return HTTP.post(
+          `api/market/pay`,
+          {},
+          {
+            params: {
+              token: token,
+            },
+          }
+        ).then(response => {
+          return []
+        })
       })
-    })
-    yield put(requestBasketSuccess(basket))
-  } catch (error) {
-    yield put(logOutSaga403(error))
-    yield put(requestBasketError("Ошибка сервера"))
+      yield put(requestBasketSuccess(basket))
+    } catch (error) {
+      yield put(logOutSaga403(error))
+      yield put(requestBasketError("Ошибка сервера"))
+    }
   }
 }
 
-export function* watchUpdateBasket(): IterableIterator<ForkEffect> {
-  yield takeEvery(BASKET_UPDATE, updateBasketAsync)
+export function* watchUpdateBasket() {
+  while (true) {
+    const { payload } = yield take(BASKET_UPDATE)
+    try {
+      yield put(requestBasket())
+      const token = yield select(getToken)
+      const count = yield call(() => {
+        return HTTP.post(
+          `api/market/products`,
+          {},
+          {
+            params: {
+              token: token,
+              product: payload.product,
+              count: payload.count,
+            },
+          }
+        ).then(response => {
+          return response
+        })
+      })
+      if (count.data.message === "Success") {
+        const basket = yield call(() => {
+          return HTTP.get(`api/get/backet`, {
+            params: {
+              token: token,
+            },
+          }).then(response => {
+            return response
+          })
+        })
+        if (basket.data.body !== null) {
+          yield put(requestBasketSuccess(basket.data.body))
+        } else {
+          yield put(requestBasketError("Пустое тело ответа"))
+        }
+      } else {
+        yield put(requestBasketError("Ошибка в получении продукта"))
+      }
+    } catch (error) {
+      yield put(logOutSaga403(error))
+      yield put(requestBasketError("Ошибка сервера"))
+    }
+  }
 }
 
-export function* updateBasketAsync(action: any) {
-  try {
-    yield console.log(action)
-    yield put(requestBasket())
-    const token = yield select(getToken)
-    const count = yield call(() => {
-      return HTTP.post(
-        `api/market/products`,
-        {},
-        {
-          params: {
-            token: token,
-            product: action.payload.product,
-            count: action.payload.count,
-          },
-        }
-      ).then(response => {
-        return response
-      })
-    })
-    if (count.data.message === "Success") {
+export function* watchFetchBasket() {
+  while (true) {
+    yield take(BASKET_FETCHED)
+    try {
+      yield put(requestBasket())
+      const token = yield select(getToken)
       const basket = yield call(() => {
         return HTTP.get(`api/get/backet`, {
           params: {
@@ -80,39 +106,9 @@ export function* updateBasketAsync(action: any) {
       } else {
         yield put(requestBasketError("Пустое тело ответа"))
       }
-    } else {
-      yield put(requestBasketError("Ошибка в получении продукта"))
+    } catch (error) {
+      yield put(logOutSaga403(error))
+      yield put(requestBasketError("Ошибка сервера"))
     }
-  } catch (error) {
-    yield put(logOutSaga403(error))
-    yield put(requestBasketError("Ошибка сервера"))
-  }
-}
-
-export function* watchFetchBasket(): IterableIterator<ForkEffect> {
-  yield takeEvery(BASKET_FETCHED, fetchBasketAsync)
-}
-
-function* fetchBasketAsync() {
-  try {
-    yield put(requestBasket())
-    const token = yield select(getToken)
-    const basket = yield call(() => {
-      return HTTP.get(`api/get/backet`, {
-        params: {
-          token: token,
-        },
-      }).then(response => {
-        return response
-      })
-    })
-    if (basket.data.body !== null) {
-      yield put(requestBasketSuccess(basket.data.body))
-    } else {
-      yield put(requestBasketError("Пустое тело ответа"))
-    }
-  } catch (error) {
-    yield put(logOutSaga403(error))
-    yield put(requestBasketError("Ошибка сервера"))
   }
 }
